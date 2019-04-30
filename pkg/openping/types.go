e7111433b9b4d813618eda38c9c15978e5c57a53
Ryan Hartje <ryan@ryanhartje.com>
2019-04-30 17:40:54 -0500

    add json to models

--- pkg/openping/types.go
+++ pkg/openping/types.go
@@ -4,37 +4,37 @@ import "time"
 
 // Uptime stores uptime information.
 type Uptime struct {
-	Timestamp time.Time // The time of the request
-	Up        bool      // A 200 Status code, free from common errors
-	RC        int       // Return code
-	URL       string
+	Timestamp time.Time `json:"timestamp"` // The time of the request
+	Up        bool      `json:"up"`        // A 200 Status code, free from common errors
+	RC        int       `json:"rc"`        // Return code
+	URL       string    `json:"url"`
 }
 
 // Latency contains measurements of time for page loads
 type Latency struct {
-	DNSLookup    time.Duration // Time from DNSLookupStart to DNSLookupDone
-	Timestamp    time.Time     // The time of the request
-	TLSHandshake time.Duration // Time from TLSHandshakeStart to TLSHandshakeDone
-	TotalLatency time.Duration // Time from connection to response
-	TTFB         time.Duration // Time from Connection started to FirstByte Received
-	URL          string
+	DNSLookup    time.Duration `json:"dns_lookup"`    // Time from DNSLookupStart to DNSLookupDone
+	Timestamp    time.Time     `json:"timestamp"`     // The time of the request
+	TLSHandshake time.Duration `json:"tls_handshake"` // Time from TLSHandshakeStart to TLSHandshakeDone
+	TotalLatency time.Duration `json:"total_latency"` // Time from connection to response
+	TTFB         time.Duration `json:"ttfb"`          // Time from Connection started to FirstByte Received
+	URL          string        `json:"url"`
 }
 
 // ContentSizes breaks down the sizes of various parts of a request
 type ContentSizes struct {
-	CSSSize      int // Sum of Size in bytes of all stylesheets
-	DocumentSize int // Sum of size in bytes of the fetched document
-	FontSize     int // Sum of all loaded fonts
-	ImageSize    int // Sum of Size in bytes of all jpg,jpeg,png,gif,svg,and ico links
-	ScriptSize   int // Sum of all Javascript linked script files and inline scripts
-	URL          string
+	CSSSize      int    `json:"css_size"`      // Sum of Size in bytes of all stylesheets
+	DocumentSize int    `json:"document_size"` // Sum of size in bytes of the fetched document
+	FontSize     int    `json:"font_size"`     // Sum of all loaded fonts
+	ImageSize    int    `json:"image_size"`    // Sum of Size in bytes of all jpg,jpeg,png,gif,svg,and ico links
+	ScriptSize   int    `json:"script_size"`   // Sum of all Javascript linked script files and inline scripts
+	URL          string `json:"url"`
 }
 
 // Metadata stores basic meta about a request
 type Metadata struct {
-	Bytes     int       // Size of the request
-	Document  string    // A copy of the pulled document
-	SHASum    string    // A SHA sum fingerprint of the document
-	Timestamp time.Time // The time of the request
-	URL       string
+	Bytes     int       `json:"bytes"`     // Size of the request
+	Document  string    `json:"document"`  // A copy of the pulled document
+	SHASum    string    `json:"shasum"`    // A SHA sum fingerprint of the document
+	Timestamp time.Time `json:"timestamp"` // The time of the request
+	URL       string    `json:"url"`
 }
